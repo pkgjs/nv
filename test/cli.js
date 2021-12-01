ff3514e4cfae4051ff714ee635844763f0ce0d9f
Wes Todd <wtodd@netflix.com>
2021-11-30 19:50:20 -0600

    feat: cli options mirror & pretty-json

--- test/cli.js
+++ test/cli.js
@@ -1,8 +1,7 @@
 'use strict'
-
 const assert = require('assert')
 const { suite, test } = require('mocha')
-const childProcess = require('child_process')
+const { execFileSync } = require('child_process')
 const path = require('path')
 
 const nv = path.join(__dirname, '..', 'bin', 'nv')
@@ -10,12 +9,22 @@ const cwd = path.join(__dirname, '..')
 
 suite('nv cli', () => {
   test('should contain help information', () => {
-    const result = childProcess.execSync(`${nv} --help`, { cwd: cwd }).toString()
+    const result = execFileSync(nv, ['--help'], { cwd: cwd }).toString()
     assert.ok(result.includes('List Node.js versions'))
   })
 
   test('should contain information about version 8', () => {
-    const result = childProcess.execSync(`${nv} ls 8`, { cwd: cwd }).toString()
-    assert.ok(result.includes('carbon'))
+    const result = JSON.parse(execFileSync(nv, ['ls', '8'], { cwd: cwd }).toString())
+    assert.strictEqual(result.codename, 'carbon')
+  })
+  test('should contain output newline json', () => {
+    const result = execFileSync(nv, ['ls', '8.x', '--no-pretty-json'], { cwd: cwd })
+      .toString().trim().split('\n')
+      .map((line) => JSON.parse(line))
+
+    assert(Array.isArray(result))
+    result.forEach((r) => {
+      assert(r.version.startsWith('8.'))
+    })
   })
 })

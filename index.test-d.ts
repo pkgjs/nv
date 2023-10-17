98bde290d4e677539e5d9025c4db6f9cbb409930
Wes Todd <wes@wesleytodd.com>
2023-10-17 13:55:38 -0500

    feat: add files to versions

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,7 +1,22 @@
 import nv from '.'
+import assert from 'node:assert'
 
 (async () => {
-  await nv()
+  const versions = await nv()
+  assert(versions[0].version)
+  assert(versions[0].major)
+  assert(versions[0].minor)
+  assert(versions[0].patch)
+  assert(versions[0].tag)
+  assert(versions[0].codename)
+  assert(versions[0].versionName)
+  assert(versions[0].start)
+  assert(versions[0].lts)
+  assert(versions[0].maintenance)
+  assert(versions[0].end)
+  assert(versions[0].releaseDate)
+  assert(versions[0].isLts)
+  assert(versions[0].files)
   await nv('lts_active')
   await nv(['lts_active', 'supported'])
   await nv(['lts_active'], {

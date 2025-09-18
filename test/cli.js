c67d33832e50946bb01a5a2f504285564de18d3a
Wes Todd <wes@wesleytodd.com>
2025-09-18 10:14:07 -0500

    fix(dev-deps): updated mocha, standard, & tsd

--- test/cli.js
+++ test/cli.js
@@ -9,16 +9,16 @@ const cwd = path.join(__dirname, '..')
 
 suite('nv cli', () => {
   test('should contain help information', () => {
-    const result = execFileSync(nv, ['--help'], { cwd: cwd }).toString()
+    const result = execFileSync(nv, ['--help'], { cwd }).toString()
     assert.ok(result.includes('List Node.js versions'))
   })
 
   test('should contain information about version 8', () => {
-    const result = JSON.parse(execFileSync(nv, ['ls', '8'], { cwd: cwd }).toString())
+    const result = JSON.parse(execFileSync(nv, ['ls', '8'], { cwd }).toString())
     assert.strictEqual(result.codename, 'carbon')
   })
   test('should contain output newline json', () => {
-    const result = execFileSync(nv, ['ls', '8.x', '--no-pretty-json'], { cwd: cwd })
+    const result = execFileSync(nv, ['ls', '8.x', '--no-pretty-json'], { cwd })
       .toString().trim().split('\n')
       .map((line) => JSON.parse(line))
 
@@ -28,7 +28,7 @@ suite('nv cli', () => {
     })
   })
   test('should only contain the latest of each major', () => {
-    const result = execFileSync(nv, ['ls', '16.x || 18.x', '--no-pretty-json', '--latest-of-major-only'], { cwd: cwd })
+    const result = execFileSync(nv, ['ls', '16.x || 18.x', '--no-pretty-json', '--latest-of-major-only'], { cwd })
       .toString().trim().split('\n')
       .map((line) => JSON.parse(line))
 
